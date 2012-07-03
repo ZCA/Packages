c4ba941a43f0f9e971ed9366d7b7d763eeb1e2f5
David Petzel <davidpetzel@gmail.com>
2012-07-02 21:36:07 -0400

    fixing it up for some environment variables on execute

--- chef-repo/cookbooks/zca_build_server/recipes/run_build.rb
+++ chef-repo/cookbooks/zca_build_server/recipes/run_build.rb
@@ -17,10 +17,32 @@
 # limitations under the License.
 #
 
+=begin
+if node['platform'] == 'ubuntu' && node['platform_version'].split(".")[0] then
+#I think the following hackery is likely a bad idea, but its the only way I could get this to work right
+#Maybe some day there will be a better fix
+#http://jira.zenoss.com/jira/browse/ZEN-1099?focusedCommentId=15072&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-15072
+#Given that nonsense, apply some file hackery
+execute "install.sh" do
+	command "sed -i 's/threadsafe = True/threadsafe = False/' build/MySQL-python-1.2.3/site.cfg"
+	cwd
+	user  "zenoss"
+	action :run
+end
+
+=end
+
+
+
 #Once everything in place lets kick off the build process automatically
 execute "install.sh" do
-  command "install.sh --no-prompt"
+  command "./install.sh --no-prompt"
   cwd "/home/zenoss/install-sources"
   user  "zenoss"
   action :run
+  environment ({'ZENHOME' => '/opt/zenoss',
+				'PYTHONPATH' => '$ZENHOME/lib/python',
+				'PATH' => '/usr/bin/:/bin:$ZENHOME/bin:$PATH:/usr/lib/jvm/jdk1.6.0_31/bin/',
+				'INSTANCE_HOME' => '$ZENHOME'
+			  })
 end
